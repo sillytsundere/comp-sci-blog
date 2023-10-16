d314961bf2297b511c7b467c246cffad517723a0
Paige <paigehcarroll@gmail.com>
2023-10-16 15:13:09 -0500

    removed comment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    //what is JAWSDB_URL?
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
@@ -13,9 +12,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
+      host: "127.0.0.1",
+      dialect: "mysql",
+      port: 3306,
     }
   );
 }
